4f5652cfe8c82d7d97d367f9841eea81d7d657e3
Akshat <70918043+ftaxats@users.noreply.github.com>
2024-12-15 19:19:28 +0530

    onborading

--- apps/web/utils/config.ts
+++ apps/web/utils/config.ts
@@ -1,6 +1,6 @@
 export const AI_GENERATED_FIELD_VALUE = "___AI_GENERATE___";
 
-export const appHomePath = "/automation";
+export const appHomePath = "/onboarding";
 
 export const GroupName = {
   NEWSLETTER: "Newsletters",
