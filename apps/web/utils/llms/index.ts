d6b44ce543f10f6bb95058945d50b2661dfd9cfe
Akshat <70918043+ftaxats@users.noreply.github.com>
2024-12-15 15:02:15 +0530

    bedrock openai issue solved by using openai key as default from .env

--- apps/web/utils/llms/index.ts
+++ apps/web/utils/llms/index.ts
@@ -9,14 +9,12 @@ import {
 } from "ai";
 import { createOpenAI } from "@ai-sdk/openai";
 import { createAnthropic } from "@ai-sdk/anthropic";
-import { createAmazonBedrock } from "@ai-sdk/amazon-bedrock";
 import { env } from "@/env";
 import { saveAiUsage } from "@/utils/usage";
 import { Model, Provider } from "@/utils/llms/config";
 import type { UserAIFields } from "@/utils/llms/types";
 import { addUserErrorMessage, ErrorType } from "@/utils/error-messages";
 import {
-  isAnthropicInsufficientBalanceError,
   isIncorrectOpenAIAPIKeyError,
   isInvalidOpenAIModelError,
   isOpenAIAPIKeyDeactivatedError,
@@ -25,7 +23,7 @@ import {
 import { sleep } from "@/utils/sleep";
 
 function getModel({ aiProvider, aiModel, aiApiKey }: UserAIFields) {
-  const provider = aiProvider || Provider.ANTHROPIC;
+  const provider = aiProvider || Provider.OPEN_AI;
 
   if (provider === Provider.OPEN_AI) {
     const model = aiModel || Model.GPT_4O;
@@ -37,33 +35,13 @@ function getModel({ aiProvider, aiModel, aiApiKey }: UserAIFields) {
   }
 
   if (provider === Provider.ANTHROPIC) {
-    if (aiApiKey) {
-      const model = aiModel || Model.CLAUDE_3_5_SONNET_ANTHROPIC;
-      return {
-        provider: Provider.ANTHROPIC,
-        model,
-        llmModel: createAnthropic({ apiKey: aiApiKey })(model),
-      };
-    }
-    if (!env.BEDROCK_ACCESS_KEY)
-      throw new Error("BEDROCK_ACCESS_KEY is not set");
-    if (!env.BEDROCK_SECRET_KEY)
-      throw new Error("BEDROCK_SECRET_KEY is not set");
-
-    const model = aiModel || Model.CLAUDE_3_5_SONNET_BEDROCK;
-
+    const model = aiModel || Model.CLAUDE_3_5_SONNET_ANTHROPIC;
     return {
       provider: Provider.ANTHROPIC,
       model,
-      llmModel: createAmazonBedrock({
-        bedrockOptions: {
-          region: env.BEDROCK_REGION,
-          credentials: {
-            accessKeyId: env.BEDROCK_ACCESS_KEY,
-            secretAccessKey: env.BEDROCK_SECRET_KEY,
-          },
-        },
-      })(model),
+      llmModel: createAnthropic({ apiKey: aiApiKey || env.ANTHROPIC_API_KEY })(
+        model,
+      ),
     };
   }
 
@@ -198,52 +176,6 @@ export async function chatCompletionTools({
   }
 }
 
-// not in use atm
-async function streamCompletionTools({
-  userAi,
-  prompt,
-  system,
-  tools,
-  maxSteps,
-  userEmail,
-  label,
-  onFinish,
-}: {
-  userAi: UserAIFields;
-  prompt: string;
-  system?: string;
-  tools: Record<string, CoreTool>;
-  maxSteps?: number;
-  userEmail: string;
-  label: string;
-  onFinish?: (text: string) => Promise<void>;
-}) {
-  const { provider, model, llmModel } = getModel(userAi);
-
-  const result = await streamText({
-    model: llmModel,
-    tools,
-    toolChoice: "required",
-    prompt,
-    system,
-    maxSteps,
-    experimental_telemetry: { isEnabled: true },
-    onFinish: async ({ usage, text }) => {
-      await saveAiUsage({
-        email: userEmail,
-        provider,
-        model,
-        usage,
-        label,
-      });
-
-      if (onFinish) await onFinish(text);
-    },
-  });
-
-  return result;
-}
-
 export async function withRetry<T>(
   fn: () => Promise<T>,
   {
@@ -311,14 +243,6 @@ async function handleError(error: unknown, userEmail: string) {
       return await addUserErrorMessage(
         userEmail,
         ErrorType.OPENAI_RETRY_ERROR,
-        error.message,
-      );
-    }
-
-    if (isAnthropicInsufficientBalanceError(error)) {
-      return await addUserErrorMessage(
-        userEmail,
-        ErrorType.ANTHROPIC_INSUFFICIENT_BALANCE,
         error.message,
       );
     }
