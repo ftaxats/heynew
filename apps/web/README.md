067a803893f9faa90a2db50b23fd763443569086
Akshat <70918043+ftaxats@users.noreply.github.com>
2025-01-01 19:53:45 +0530

    Your commit message

---
+++ apps/web/README.md
@@ -0,0 +1,9 @@
+[![](apps/web/app/opengraph-image.png)](https://mailto.live)
+
+<p align="center">
+  <a href="https://mailto.live">
+    <h1 align="center">Mailto Live</h1>
+  </a>
+  <p align="center">
+   
+    <br />
---
+++ apps/web/apps/web/app/sitemap.ts
@@ -0,0 +1,87 @@
+import type { MetadataRoute } from "next";
+import { unstable_noStore } from "next/cache";
+import { sanityFetch } from "@/sanity/lib/fetch";
+import { postSlugsQuery } from "@/sanity/lib/queries";
+
+async function getBlogPosts() {
+  const posts = await sanityFetch<{ slug: string; date: string }[]>({
+    query: postSlugsQuery,
+  });
+  return posts.map((post) => ({
+    url: `https://mailto.live/blog/post/${post.slug}`,
+    lastModified: new Date(post.date),
+  }));
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // to try fix caching issue: https://github.com/vercel/next.js/discussions/56708#discussioncomment-10127496
+  unstable_noStore();
+
+  const blogPosts = await getBlogPosts();
+
+  const staticUrls = [
+    {
+      url: "https://mailto.live/",
+      priority: 1,
+    },
+    {
+      url: "https://mailto.live/bulk-email-unsubscriber",
+    },
+    {
+      url: "https://mailto.live/ai-automation",
+    },
+    {
+      url: "https://mailto.live/email-analytics",
+    },
+    {
+      url: "https://mailto.live/block-cold-emails",
+    },
+    {
+      url: "https://mailto.live/new-email-senders",
+    },
+    {
+      url: "https://mailto.live/waitlist",
+    },
+    {
+      url: "https://mailto.live/privacy",
+    },
+    {
+      url: "https://mailto.live/terms",
+    },
+    {
+      url: "https://mailto.live/blog",
+      changeFrequency: "daily",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://mailto.live/blog/post/alternatives-to-skiff-mail",
+    },
+    {
+      url: "https://mailto.live/blog/post/best-email-unsubscribe-app",
+    },
+    {
+      url: "https://mailto.live/blog/post/bulk-unsubscribe-from-emails",
+    },
+    {
+      url: "https://mailto.live/blog/post/escape-email-trap-unsubscribe-for-good",
+    },
+    {
+      url: "https://docs.mailto.live/",
+    },
+    {
+      url: "https://docs.mailto.live/introduction",
+    },
+    {
+      url: "https://docs.mailto.live/email-ai-automation",
+    },
+    {
+      url: "https://docs.mailto.live/bulk-email-unsubscriber",
+    },
+    {
+      url: "https://docs.mailto.live/cold-email-blocker",
+    },
+  ];
+
+  return [...staticUrls, ...blogPosts];
+}
